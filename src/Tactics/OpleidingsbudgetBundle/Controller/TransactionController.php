c44864d71547e078e48e1671babefe815c67c6dd
jonasdw <jonas.dewint@gmail.com>
2014-07-02 15:10:41 +0200

    Transaction list per user

--- src/Tactics/OpleidingsbudgetBundle/Controller/TransactionController.php
+++ src/Tactics/OpleidingsbudgetBundle/Controller/TransactionController.php
@@ -23,10 +23,21 @@ class TransactionController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('TacticsOpleidingsbudgetBundle:Transaction')->findAll();
+        //GET CURRENT USER
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        //GET ALL TRANSACTIONS
+        //$entities = $em->getRepository('TacticsOpleidingsbudgetBundle:Transaction')->findAll();
+
+        //GET TRANSACTIONS PER current USER | sort by date
+        $entities = $em->getRepository('TacticsOpleidingsbudgetBundle:Transaction')->findBy(
+            array('user_id' => $user->getId()),
+            array('date' => 'ASC')
+        );
 
         return $this->render('TacticsOpleidingsbudgetBundle:Transaction:index.html.twig', array(
             'entities' => $entities,
+            'user' => $user
         ));
     }
     /**
