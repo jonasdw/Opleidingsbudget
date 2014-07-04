9e28ca993166a9719975f84b2a1ee78f58b67a07
jonasdw <jonas.dewint@gmail.com>
2014-07-04 16:52:14 +0200

    ExpenseRequest Repository

---
+++ src/Tactics/OpleidingsbudgetBundle/Repository/ExpenseRequestRepository.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonas_000
+ * Date: 4/07/14
+ * Time: 16:27
+ */
+namespace Tactics\OpleidingsbudgetBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class ExpenseRequestRepository extends EntityRepository
+{
+    public function getExpenseRequestWithUsers()
+    {
+
+
+    }
+}
