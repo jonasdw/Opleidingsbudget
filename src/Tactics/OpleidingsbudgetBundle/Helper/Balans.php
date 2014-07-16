38bdc11086534c1b85f9372f181ef375b3600213
jonasdw <jonas.dewint@gmail.com>
2014-07-16 14:15:33 +0200

    Calculate user balans correct

--- src/Tactics/OpleidingsbudgetBundle/Helper/Balans.php
+++ src/Tactics/OpleidingsbudgetBundle/Helper/Balans.php
@@ -8,20 +8,22 @@ use Money\Currency;
 class Balans
 {
     private $transactions;
-    private $balans = 0;
+    private $balans;
 
     public function __construct($transactions)
     {
         $this->transactions = $transactions;
-
     }
 
     public function getUserBalans()
     {
+        $this->balans = Money::EUR(0);
+
         foreach ($this->transactions as $transaction){
-            $this->balans += floatval($transaction->getAmount()->getAmount());
+
+            $this->balans = $this->balans->add($transaction->getAmount());
         }
 
-        return Money::EUR((int)$this->balans);
+        return $this->balans;
     }
 }
